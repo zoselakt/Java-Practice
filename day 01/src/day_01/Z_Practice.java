48b69181e132cfedfd05fd805597406a62ed64e7
zoselakt <kd66@naver.com>
2022-02-15 20:33:01 +0900

    thirteen commit

--- day 01/src/day_01/Z_Practice.java
+++ day 01/src/day_01/Z_Practice.java
@@ -1,7 +1,21 @@
 package day_01;
 
 public class Z_Practice{
+	
 	public static void main(String[] args) {
-		
+		int intArray[] = {1,2,0,4,5};
+		try {			
+			String str = null;
+			System.out.println(intArray[0]);
+//			System.out.println(intArray[4] / intArray[2]);
+			str.toString();
+			System.out.println(intArray[5]);
+			return;
+		}catch(Exception e) {
+			System.out.println("예외발생 " + e.getMessage());
+		}
+		finally {
+			System.out.println("항상 실행되는 코드");
+		}
 	}
 }
--- day 01/src/day_01/Z_Practice1.java
+++ day 01/src/day_01/Z_Practice1.java
@@ -3,7 +3,17 @@ package day_01;
 
 public class Z_Practice1 {
 	public static void main(String[] args) {
-		
+		int intArray[] = {1,2,3,4,5};
+		try {			
+			System.out.println(intArray[0]);
+			System.out.println(intArray[5]);
+			return;
+		}catch(Exception e) {
+			System.out.println("intArray배열의 크기는" + intArray.length+"입니다.");
+		}
+		finally {
+			System.out.println("항상 실행되는 코드");
+		}
 	}
 }
 
